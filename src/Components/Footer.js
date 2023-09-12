5ef27600c4018c6ea513b70caea7048a872b145d
Camilo <blindninja305@gmail.com>
2023-09-11 22:04:15 -0700

    setting up footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 
+
 function Footer() {
 	return (
-		<div>
-			<section>
+		<div className='footer'>
+			<section className='footer-column'>
 				<h2>Moviemax</h2>
+				<Link to='/'>Home</Link>
+				<Link to='/about'>About</Link>
+				<Link to='/favorites'>Favorites</Link>
 			</section>
-			<Link to='/'>Home</Link>
-			<Link to='/about'>About</Link>
-			<Link to='/favorites'>Favorites</Link>
+			<section className='footer-column'>
+				<Link to='/contact'>Contact</Link>
+				<Link to='/help'>Help</Link>
+				<Link to='/media'>Media</Link>
+			</section>
 		</div>
 	);
 }
