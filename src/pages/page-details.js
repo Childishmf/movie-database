ec417c794854349e8ec91bfa602ca3a0b386bd79
mohamed7135 <55821812+mohamed7135@users.noreply.github.com>
2023-09-24 20:56:50 -0700

    video updated size

--- src/pages/page-details.js
+++ src/pages/page-details.js
@@ -45,6 +45,8 @@ const PageDetails = () => {
         <p>{movieDetail?.release_date}</p>
         {/* <p>2023-06-02</p> */}
         <h2>Overview</h2>
+        <button className='favs' type="button" onclick="alert('Hello world!')">Favorite</button>
+
         <p>{movieDetail?.overview}</p>
         {/* <p>//////////After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.</p> */}
         </div>
@@ -56,8 +58,16 @@ const PageDetails = () => {
           />
         </div>
       </div>
-        
-        <YoutubeEmbed embedId="cqGjhVJWtEg"/>
+      <iframe
+      className='youtubeEmbed'
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/VIDEO_ID`}
+            title="YouTube Video Player"
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
+        {/* <YoutubeEmbed embedId="cqGjhVJWtEg"/> */}
         <p>below youtube</p>
     </div>
   );
