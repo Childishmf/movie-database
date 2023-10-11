7be6d32ce49c6057331285e0753e67f6ca934019
Camilo <blindninja305@gmail.com>
2023-10-11 14:46:30 -0700

    set up logo and styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navigation from './Navigation'; // Import your Navigation component
 import MobileNavigation from './MobileNavigation'; // Import your MobileNavigation component
+import image from '../images/movie-cat.svg';
 
 const Header = () => {
 	return (
 		<header>
+			<div className='logo'>
+				<img src={image} alt='logo' />
+			</div>
 			<Navigation /> {/* Render your Navigation component */}
 		</header>
 	);
