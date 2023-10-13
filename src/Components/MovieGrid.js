5cb03defdbd12ab7e7d2e1cd02791333ab4b2ecb
Rane-19 <dia1929@yahoo.com.ph>
2023-10-13 12:39:27 -0700

    edit

--- src/Components/MovieGrid.js
+++ src/Components/MovieGrid.js
@@ -1,31 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import setFavourites from './SetFavourites';
-
+import { favouritesKey } from '../globals/constants';
 
 function MovieGrid() {
   const [movies, setMovies] = useState([]);
-<<<<<<< HEAD
   const [sortingOption, setSortingOption] = useState('popular');
   const [hoveredMovie, setHoveredMovie] = useState(null);
-=======
->>>>>>> 040727d12642046877e4a25297207daaf48cf10d
   const navigate = useNavigate();
-  const onMoreInfo = (id) => navigate('/details', {state: {movieId: id}});
-  // const test = (id) => console.log(id);
-  const [selected, setSelected] = useState('popular');
-  
-  const buttonSelect = (e) => {
-    let id = e.target.id;
-    id === 'popular' 
-    ? setSelected('popular')
-    : id === 'top-rated'
-    ? setSelected('top-rated')
-    : id === 'now-playing'
-    ? setSelected('now-playing')
-    : setSelected('upcoming');
+
+  function addFavourites(id) {
+    let favourites = JSON.parse(localStorage.getItem(favouritesKey));
+
+    if (favourites == null) {
+      favourites = [];
+    }
+
+    favourites = [...favourites, id];
+    let favouritesAsJson = JSON.stringify(favourites);
+    localStorage.setItem(favouritesKey, favouritesAsJson);
   }
-<<<<<<< HEAD
 
   const onMoreInfo = (id) => {
     navigate(`/details/${id}`);
@@ -35,8 +28,6 @@ function MovieGrid() {
     setSortingOption(event.target.value);
   };
 
-=======
->>>>>>> 040727d12642046877e4a25297207daaf48cf10d
   useEffect(() => {
     const options = {
       method: 'GET',
@@ -50,55 +41,47 @@ function MovieGrid() {
       try {
         let endpoint = '';
 
-        selected === 'popular'
-        ? endpoint = 'popular'
-        : selected === 'top-rated'
-        ? endpoint = 'top_rated'
-        : selected === 'now-playing'
-        ? endpoint = 'now_playing'
-        : endpoint = 'upcoming';
-        const response = await fetch(
-                `https://api.themoviedb.org/3/movie/${endpoint}?language=en-US&page=1`,
-                options
-              );
-        if (!response.ok) {
-        throw new Error('Network response was not ok');
+        if (sortingOption === 'popular') {
+          endpoint = 'now_playing';
+        } else if (sortingOption === 'best_rated') {
+          endpoint = 'top_rated';
+        } else if (sortingOption === 'now_playing') {
+          endpoint = 'now_playing';
+        } else if (sortingOption === 'upcoming') {
+          endpoint = 'upcoming';
         }
+
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${endpoint}?language=en-US&page=1`,
+          options
+        );
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
         const data = await response.json();
         setMovies(data.results.slice(0, 12));
-      }
-      catch (error) {
+      } catch (error) {
         console.error('Error fetching data:', error);
       }
-    }
+    };
+
     getMovies();
-  }, [selected]);
+  }, [sortingOption]);
 
   return (
-    <div className="movie-wrapper">
-      <div className='radio-wrapper'>
-              <button id='popular' className={selected === "popular" ? "selected radio-btn" : "radio-btn"} onClick={buttonSelect}>Popular</button>
-              <button id='top-rated' className={selected === "top-rated" ? "selected radio-btn" : "radio-btn"} onClick={buttonSelect}>Top Rated</button>
-              <button id='now-playing' className={selected === "now-playing" ? "selected radio-btn" : "radio-btn"} onClick={buttonSelect}>Now Playing</button>
-              <button id='upcoming' className={selected === "upcoming" ? "selected radio-btn" : "radio-btn"} onClick={buttonSelect}>Upcoming</button>
-      </div>
-      <div className="movie-grid">
-        {movies.map((movie) => (
-          <div key={movie.id} className="movie-card">
-            <img
-              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-              alt={movie.title}
-            />
-            <h2>{movie.title}</h2>
-            <p>{movie.release_date}</p>
-            <button className='infoBtn' type="button" onClick={() => onMoreInfo(movie.id)}>More Info</button>
-            <br />
-            <button className='favsBtn' type="button" onClick={() => setFavourites(movie.id)}>Favorite</button>
-          </div>
-        ))}
+    <div className="movie-grid">
+      <div>
+        <label>Sort By:</label>
+        <select onChange={handleSortingChange} value={sortingOption}>
+          <option value="popular">Popular</option>
+          <option value="best_rated">Best Rated</option>
+          <option value="now_playing">Now Playing</option>
+          <option value="upcoming">Upcoming</option>
+        </select>
       </div>
 
-<<<<<<< HEAD
       {movies.map((movie) => (
         <div
           key={movie.id}
@@ -134,9 +117,6 @@ function MovieGrid() {
           )}
         </div>
       ))}
-=======
-    
->>>>>>> 040727d12642046877e4a25297207daaf48cf10d
     </div>
   );
 }
